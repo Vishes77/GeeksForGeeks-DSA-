f97f075ff6de655b3684c712f180379375529e0f
princ <princearyangf@gmail.com>
2020-09-28 19:57:05 +0530

    Solution added.

--- Arrays/Arry insert at end.py
+++ Arrays/Arry insert at end.py
@@ -3,6 +3,44 @@
 # ou are given an array arr. The size of the array is given by sizeOfArray.
 # You need to insert an element at the end.
 
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for python3
+
+# You only need to insert the given element at
+# the end, i.e., at index sizeOfArray - 1. You may
+# assume that the array already has sizeOfArray - 1
+# elements.
+def insertAtEnd(arr, sizeOfArray, element):
+    arr.append(element)
+    #the complexity of append is O(1).
+
+def main():
+    testcases = int(input())
+
+    while (testcases > 0):
+        sizeOfArray = int(input())
+
+        arr = [int(x) for x in input().strip().split()]
+
+        element = int(input())
+
+        insertAtEnd(arr, sizeOfArray, element)
+
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+        testcases -= 1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
 
 
 
