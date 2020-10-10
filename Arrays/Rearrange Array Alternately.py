f95df8f9a699f07c4d3db288f2cf28c987bd0b15
princ <princearyangf@gmail.com>
2020-10-10 20:45:18 +0530

    Solution Added

--- Arrays/Rearrange Array Alternately.py
+++ Arrays/Rearrange Array Alternately.py
@@ -11,9 +11,35 @@ def rearrange(arr, n):
         final.append(arr[i])
         k -= 1
     for i in range(n):
-        arr[i] = final[i]  # for this complexity is 3.33
+        arr[i] = final[i]  # for this complexity is 3.33 AND O(n) SPACE COMPLEXITY.
 
-
+    #ALTERNATIVE OF THIS WITH O(1) SPACE COMPLEXITY AND O(N) TIME COMPLEXITY
+    # def rearrange(arr, n):
+    #
+    #     # Initialize index of first minimum
+    #     # and first maximum element
+    #     max_idx = n - 1
+    #     min_idx = 0
+    #
+    #     # Store maximum element of array
+    #     max_elem = arr[n - 1] + 1
+    #
+    #     # Traverse array elements
+    #     for i in range(0, n):
+    #
+    #         # At even index : we have to put maximum element
+    #         if i % 2 == 0:
+    #             arr[i] += (arr[max_idx] % max_elem) * max_elem
+    #             max_idx -= 1
+    #
+    #         # At odd index : we have to put minimum element
+    #         else:
+    #             arr[i] += (arr[min_idx] % max_elem) * max_elem
+    #             min_idx += 1
+    #
+    #     # array elements back to it's original form
+    #     for i in range(0, n):
+    #         arr[i] = arr[i] // max_elem
 
 
 def main():
