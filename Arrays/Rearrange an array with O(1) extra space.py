1168035d3e8d9445de510788fee6af747512f55c
princ <princearyangf@gmail.com>
2020-10-11 11:23:48 +0530

    Solution Added

--- Arrays/Rearrange an array with O(1) extra space.py
+++ Arrays/Rearrange an array with O(1) extra space.py
@@ -2,12 +2,13 @@
 
 ##Complete this code
 def arrange(arr, n):
-    new = list()
+    new = [0]*n
     for i in range(n):
         new[i] = arr[arr[i]]
     for i in range(n):
         arr[i] = new[i]
-    return arr
+    return arr        #complexity os o(N) time = 0.03
+                      #but space complexity is o(N).
 
 import math
 
