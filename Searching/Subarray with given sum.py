802dc7f91f9e6423baa08e55829aa6e1be319c8e
princ <princearyangf@gmail.com>
2020-10-22 19:26:08 +0530

    Solution Added

--- Searching/Subarray with given sum.py
+++ Searching/Subarray with given sum.py
@@ -2,21 +2,42 @@
 
 # Complete this function
 def subArraySum(arr, n, sum):
+    # Initialize curr_sum as
+    # value of first element
+    # and starting point as 0
     curr_sum = arr[0]
     start = 0
 
-    i=1
-    while(i<=n):
-        while(curr_sum > sum and start < i-1):
+    # Add elements one by
+    # one to curr_sum and
+    # if the curr_sum exceeds
+    # the sum, then remove
+    # starting element
+    i = 1
+    while i <= n:
+
+        # If curr_sum exceeds
+        # the sum, then remove
+        # the starting elements
+        while curr_sum > sum and start < i - 1:
             curr_sum = curr_sum - arr[start]
             start += 1
+
+        # If curr_sum becomes
+        # equal to sum, then
+        # return true
         if curr_sum == sum:
             print("%d %d" % (start + 1, i), end=" ")
             return 1
-        if i<n:
-            curr_sum =curr_sum + arr[i]
+
+        # Add this element
+        # to curr_sum
+        if i < n:
+            curr_sum = curr_sum + arr[i]
         i += 1
 
+    # If we reach here,
+    # then no subarray
     print("-1", end=" ")
 
 
