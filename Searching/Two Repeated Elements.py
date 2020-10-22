79e410ed09703251f71572a23c5c65de9ae0aede
princ <princearyangf@gmail.com>
2020-10-22 12:47:33 +0530

    Solution Added

--- Searching/Two Repeated Elements.py
+++ Searching/Two Repeated Elements.py
@@ -2,7 +2,7 @@ import math
 
 def twoRepeated(arr, N):
     virtual = [0]*(N+1)
-    for i in range(N+2):
+    for i in range(N+2):        #my solution.
         virtual[arr[i]] += 1
         if(virtual[arr[i]]== 2):
             print(arr[i],end=" ")
